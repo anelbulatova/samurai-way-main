b9d8c3ddb5fc367dd0ade125f3abad5b22f6ecf2
Anel <anel.bulatova7@gmail.com>
2023-03-22 16:51:27 +0300

    first changes

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from "./Components/Header";
+import Navigation from "./Components/Navigation";
+import Profile from "./Components/Profile";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Navigation/>
+            <Profile/>
+        </div>
+
+
+    );
+}
+
+export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <img src='https://www.woodstock-guitars.com/wp-content/uploads/2017/03/new-logo-small.png'/>
+        </header>
+    )
+}
+
+export default Header;
