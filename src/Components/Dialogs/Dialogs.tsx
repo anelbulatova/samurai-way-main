61b8444c88fc1453cc0157e062615a85cd155f2b
Anel <anel.bulatova7@gmail.com>
2023-04-01 17:14:15 +0300

    'dialog page layout'

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -2,10 +2,28 @@ import React from "react";
 import s from './Dialogs.module.css'
 const Dialogs = () => {
     return (
-        <div>
-
-
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + '' + s.active}>
+                      Anelya
+                </div>
+                <div className={s.dialog}>
+                    Armin
+                </div>
+                <div className={s.dialog}>
+                    Azra
+                </div>
+                <div className={s.dialog}>
+                    Antalya
+                </div>
+            </div >
+            <div className={s.messages}>
+                <div className='message'>Hi</div>
+                <div className='message'>How are you</div>
+                <div className='message'>Fine,thanks</div>
+            </div>
         </div>
+
 
     )
 }
